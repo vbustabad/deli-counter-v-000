08395e6d6c6f92a6777eaa55c1055394146f8810
Veronica Bustabad <vbustabad@gmail.com>
2018-02-13 03:21:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,12 @@ def line(katz_deli)
     puts "The line is currently empty."
   else katz_deli.length > 0
     counter = 0
-    new_line = katz_deli.each_with_index do |name, index|
-      puts "The line is currently: #{index + 1}. #{name}"
+    katz_deli.each_with_index do |name, index|
+      new_customer = "The line is currently: #{index + 1}. #{name}"
       counter += 1
+      new_line_of_customers << new_customer
     end
-    new_line
+    new_line_of_customers
   end
 end
 
