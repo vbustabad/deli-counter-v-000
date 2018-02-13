488cfddbed04c84804b6400c159609a7d4488039
Veronica Bustabad <vbustabad@gmail.com>
2018-02-13 02:13:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,14 +8,16 @@ def line(katz_deli)
     katz_deli.each do |name, index|
       puts "The line is currently: #{index + 1}. #{name}"
       counter += 1
+      new_line = 
     end
+  katz
   end
 end
 
 def take_a_number(katz_deli, name)
   if katz_deli.length == 0
     "Welcome, #{name}. You are number #{index + 1} in line."
-  else katz_deli.push
+  else katz_deli.length > 0
     katz_deli.each_with_index do |name, index|
       line_with_new_customer = "Welcome, #{name}. You are number #{index + 1} in line."
       list_of_all_customers << line_with_new_customer
